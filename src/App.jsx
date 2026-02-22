fcfae2fdf028a2b2990641a50a579d8ed35daf41
Rakotoniaina Herizo Ralf Lionel <ralflionel120@gmail.com>
2026-02-22 07:20:50 +0300

    refactor: restructure app layout and enhance modal UX - Isolate Header, Main, and Footer components for better modularity - Add a close icon (SVG) to the confirmation modal for improved exit intent - Standardize full-width layout with sticky header and isolated scroll area - Clean up App.jsx by delegating UI sections to specific sub-containers - Improve global visual hierarchy and accessibility

--- src/App.jsx
+++ src/App.jsx
@@ -262,8 +262,8 @@ function App() {
           }`}
         >
           <div className="max-w-350 mx-auto  md:px-8 p-4">
-            <div >
-              <div className="grid grid-cols-2 gap-3 md:items-end" >
+            <div>
+              <div className="grid grid-cols-2 gap-3 md:items-end">
                 <div className="">
                   <label className="block text-xs font-bold uppercase text-slate-900 mb-2">
                     Filtrer par libelle
@@ -384,9 +384,7 @@ function App() {
                     <h3 class="text-lg font-semibold text-slate-900 ">
                       Nouvelle opération
                     </h3>
-                    <p class=" text-sm ">
-                      Gérez vos comptes facilement.
-                    </p>
+                    <p class=" text-sm ">Gérez vos comptes facilement.</p>
                   </div>
                 </div>
               </div>
@@ -492,7 +490,6 @@ function App() {
           </aside>
 
           <main className="lg:col-span-3 overflow-x-auto">
-            
             <div className="table-parent-container bg-slate-200 rounded overflow-hidden shadow-sm ">
               <div className="print-only">
                 <h2 className="text-center mb-4">
@@ -662,7 +659,10 @@ function App() {
                   {!loading && transactions.items.length > 0 && (
                     <tfoot>
                       <tr className="bg-slate-900 text-white font-bold">
-                        <td colSpan="2" className="rounded-bl px-6 py-5 text-sm bold">
+                        <td
+                          colSpan="2"
+                          className="rounded-bl px-6 py-5 text-sm bold"
+                        >
                           TOTAL
                         </td>
                         <td className="px-6 py-5 text-right text-emerald-400 bold">
@@ -691,17 +691,68 @@ function App() {
       </div>
       {deleteModal.isOpen && (
         <div className="fixed inset-0 z-40 flex items-center justify-center bg-slate-900/50 p-4 no-print">
-          <div className="w-full max-w-md overflow-hidden bg-white shadow-xl border border-slate-200">
+          <div className="w-full max-w-md overflow-hidden bg-white shadow-xl border border-slate-200 rounded">
             <div className="border-b border-slate-200 px-5 py-4">
-              <h3 className="text-base font-bold uppercase text-slate-900">
-                Confirmer la suppression
-              </h3>
-              <p className="mt-2 text-sm text-slate-600">
-                Voulez-vous vraiment supprimer l&apos;operation
+              <div class="border-b border-slate-200 pb-4 flex items-center gap-3 bg-slate-50">
+                <div class="bg-rose-500 flex items-center justify-center p-3 rounded-xl ">
+                  <svg
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      className="fill-slate-900"
+                      d="M14.7223 12.7585C14.7426 12.3448 14.4237 11.9929 14.01 11.9726C13.5963 11.9522 13.2444 12.2711 13.2241 12.6848L12.9999 17.2415C12.9796 17.6552 13.2985 18.0071 13.7122 18.0274C14.1259 18.0478 14.4778 17.7289 14.4981 17.3152L14.7223 12.7585Z"
+                    />
+                    <path
+                      className="fill-slate-900"
+                      d="M9.98802 11.9726C9.5743 11.9929 9.25542 12.3448 9.27577 12.7585L9.49993 17.3152C9.52028 17.7289 9.87216 18.0478 10.2859 18.0274C10.6996 18.0071 11.0185 17.6552 10.9981 17.2415L10.774 12.6848C10.7536 12.2711 10.4017 11.9522 9.98802 11.9726Z"
+                    />
+                    <path
+                      className="fill-slate-900"
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M10.249 2C9.00638 2 7.99902 3.00736 7.99902 4.25V5H5.5C4.25736 5 3.25 6.00736 3.25 7.25C3.25 8.28958 3.95503 9.16449 4.91303 9.42267L5.54076 19.8848C5.61205 21.0729 6.59642 22 7.78672 22H16.2113C17.4016 22 18.386 21.0729 18.4573 19.8848L19.085 9.42267C20.043 9.16449 20.748 8.28958 20.748 7.25C20.748 6.00736 19.7407 5 18.498 5H15.999V4.25C15.999 3.00736 14.9917 2 13.749 2H10.249ZM14.499 5V4.25C14.499 3.83579 14.1632 3.5 13.749 3.5H10.249C9.83481 3.5 9.49902 3.83579 9.49902 4.25V5H14.499ZM5.5 6.5C5.08579 6.5 4.75 6.83579 4.75 7.25C4.75 7.66421 5.08579 8 5.5 8H18.498C18.9123 8 19.248 7.66421 19.248 7.25C19.248 6.83579 18.9123 6.5 18.498 6.5H5.5ZM6.42037 9.5H17.5777L16.96 19.7949C16.9362 20.191 16.6081 20.5 16.2113 20.5H7.78672C7.38995 20.5 7.06183 20.191 7.03807 19.7949L6.42037 9.5Z"
+                    />
+                  </svg>
+                </div>
+                <div>
+                  <h3 class="text-lg font-semibold text-slate-900 ">
+                    Confirmer la suppression
+                  </h3>
+                  <p class=" text-sm ">Cette action est irréversible.</p>
+                </div>
+                <button
+                  type="button"
+                  class="rounded-full text-slate-50 bg-slate-800 hover:bg-slate-900 transition-all hover:cursor-pointer  text-sm w-6 h-6 ms-auto inline-flex justify-center items-center  dark:hover:text-white"
+                  onClick={closeDeleteModal}
+                >
+                  <svg
+                    class="w-2 h-2"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 14 14"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                    />
+                  </svg>
+                  <span class="sr-only">Close modal</span>
+                </button>
+              </div>
+              <p className=" p-4 md:p-5 space-y-4 mt-2 text-sm text-slate-600">
+                Voulez-vous vraiment supprimer l&apos;opération
                 {deleteModal.transactionLabel ? (
                   <span className="font-semibold text-slate-900">
                     {" "}
-                    {deleteModal.transactionLabel}
+                    {'"' + deleteModal.transactionLabel + '"'}
                   </span>
                 ) : (
                   ""
@@ -726,7 +777,7 @@ function App() {
                 type="button"
                 onClick={confirmDelete}
                 disabled={isDeleting}
-                className={`px-4 py-2 text-sm font-semibold text-white ${
+                className={`px-4 py-2 text-sm font-semibold text-white rounded ${
                   isDeleting
                     ? "bg-rose-300 cursor-not-allowed"
                     : "bg-rose-600 hover:bg-rose-700 hover:cursor-pointer"
@@ -746,20 +797,29 @@ function App() {
           </div>
         </div>
       )}
-      
 
       <footer class="bg-neutral-primary-soft rounded-base shadow-xs border border-default fixed bottom-0 w-full bg-slate-900">
-          <div class="w-full mx-auto max-w-7xl p-4 md:flex md:items-center md:justify-between">
-            <span class="text-slate-50 text-sm text-body sm:text-center">© {new Date().getFullYear()} <a href="https://flowbite.com/" class="hover:underline">Raoelison-Compte</a>. Tous droits réservés.
+        <div class="w-full mx-auto max-w-7xl p-4 md:flex md:items-center md:justify-between">
+          <span class="text-slate-50 text-sm text-body sm:text-center">
+            © {new Date().getFullYear()}{" "}
+            <a href="https://flowbite.com/" class="hover:underline">
+              Raoelison-Compte
+            </a>
+            . Tous droits réservés.
           </span>
           <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-body sm:mt-0">
             <li>
-                <a target="blank" href="https://www.linkedin.com/in/ralf-lionel-066b20227/" class="text-slate-50 hover:underline">Contactez-le développeur</a>
+              <a
+                target="blank"
+                href="https://www.linkedin.com/in/ralf-lionel-066b20227/"
+                class="text-slate-50 hover:underline"
+              >
+                Contactez-le développeur
+              </a>
             </li>
           </ul>
-          </div>
+        </div>
       </footer>
-
     </div>
   );
 }
